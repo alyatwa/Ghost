67ce24c2a84e620048f4e825e33f4851acc8634a
ali atwa <ali.atwa2040@gmail.com>
2023-08-11 15:40:03 +0300

    Update ContentBox.tsx
    start

--- apps/signup-form/src/components/ContentBox.tsx
+++ apps/signup-form/src/components/ContentBox.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 type ContentBoxProps = {
     children: React.ReactNode
-};
+}
 
 export const ContentBox: React.FC<ContentBoxProps> = ({children}) => {
     return (
         <section>
             {children}
         </section>
-    );
+    )
 };
